6f5ba1044e2aa856dffa7ddaa16c1ddd5b257159
Sebin Duke Stephan <sebindukestephan@gmail.com>
2017-03-09 22:18:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,21 +35,7 @@ function createTemplate(data){
     var title = data.title;
     var con = data.content;
     
-    var HtmlTemp = "
-    <html>  <head>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body bgcolour="light-gray">
-        <div class="container">
-           <h1>${title}</h1>
-            <hr>
-            ${content}
-        </div>
-        <script type="text/javascript" src="/ui/main.js">
-        </script>
-    </body>
-</html>
-";
+    var HtmlTemp = "<html> <head> <link href='/ui/style.css' rel='stylesheet' /></head><body bgcolour='gray'><div class='container'><center><h1>${title}</h1><hr>${con}</center></div></body></html>";
     return HtmlTemp;
 }
 
