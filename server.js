5dd46a84cf6735ffce7cc70b2507d66e419ca235
Sebin Duke Stephan <sebindukestephan@gmail.com>
2017-03-09 20:16:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ function createTemplate(data){
 
 app.get('/article/:anm', function(req,res){
     
-    pool.query("SELECT * FROM article WHERE title = '", req.params.anm,"';",function(err,result){
+    pool.query("SELECT * FROM article WHERE title = '", req.params('anm'),"';",function(err,result){
         if(err){
-            res(status(505),send(err.tostring()));
+            res(status(500),send(err.tostring()));
         }
         else
         {
@@ -53,7 +53,7 @@ app.get('/article/:anm', function(req,res){
             else{
                 //var ArticleData =result.rows[0];
                 //res.send(createTemplate(ArticleData));
-                res.send(JSON.stringify(result.rows));   
+                res.send(JSON.stringify(result));   
             }
         }
         //res.send(req.params.anm);
